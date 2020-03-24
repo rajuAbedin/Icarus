3a62c7fcc3484c3785210aafb453586dd0554830
Raju Minhajul Abedin <rajuminhajulabedin@gmail.com>
2020-03-24 12:21:46 +0000

    DataBaseConnection class created

---
+++ Icarus/DatabaseConnection.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Icarus
+{
+    public class DatabaseConnection
+    {
+        public static DataSet GetDataDB(string query)
+        {
+            using (SQLiteConnection cnn = new SQLiteConnection(ConnectionString()))
+            {
+                try
+                {
+                    var sqlAdapter = new SQLiteDataAdapter(query, cnn);
+                    var builder = new SQLiteCommandBuilder(sqlAdapter);
+                    DataSet
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+        public static void InsetDataDB(string query)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(ConnectionString()))
+            {
+                try
+                {
+                    SQLiteCommand cmd = new SQLiteCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+
+            }
+        }
+        private static string ConnectionString(string id = "Icarus")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
--- Icarus/Form1.Designer.cs
+++ Icarus/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 315);
+            this.ClientSize = new System.Drawing.Size(613, 334);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
