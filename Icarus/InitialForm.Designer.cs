f8c8c66481daf617ebc5d72c95bdbbf6159204c6
Raju Minhajul Abedin <rajuminhajulabedin@gmail.com>
2020-03-26 16:10:27 +0000

    Update

--- Icarus/InitialForm.Designer.cs
+++ Icarus/InitialForm.Designer.cs
@@ -45,6 +45,8 @@
             this.firstnameTxt = new Bunifu.UI.WinForms.BunifuTextbox.BunifuTextBox();
             this.shopNametxt = new Bunifu.UI.WinForms.BunifuTextbox.BunifuTextBox();
             this.closeButton = new Bunifu.Framework.UI.BunifuImageButton();
+            this.selectImgBtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.closeButton)).BeginInit();
             this.SuspendLayout();
@@ -65,6 +67,7 @@
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.selectImgBtn);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.contactNumberTxt);
@@ -200,7 +203,7 @@
             this.signUp.ActiveFillColor = System.Drawing.Color.SeaGreen;
             this.signUp.ActiveForecolor = System.Drawing.Color.White;
             this.signUp.ActiveLineColor = System.Drawing.Color.SeaGreen;
-            this.signUp.BackColor = System.Drawing.SystemColors.Control;
+            this.signUp.BackColor = System.Drawing.Color.White;
             this.signUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("signUp.BackgroundImage")));
             this.signUp.ButtonText = "Sign Up";
             this.signUp.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -492,10 +495,40 @@
             this.closeButton.Zoom = 5;
             this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
+            // selectImgBtn
+            // 
+            this.selectImgBtn.ActiveBorderThickness = 1;
+            this.selectImgBtn.ActiveCornerRadius = 20;
+            this.selectImgBtn.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(113)))), ((int)(((byte)(37)))));
+            this.selectImgBtn.ActiveForecolor = System.Drawing.Color.White;
+            this.selectImgBtn.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(113)))), ((int)(((byte)(37)))));
+            this.selectImgBtn.BackColor = System.Drawing.Color.White;
+            this.selectImgBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("selectImgBtn.BackgroundImage")));
+            this.selectImgBtn.ButtonText = "Select shop logo";
+            this.selectImgBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.selectImgBtn.Font = new System.Drawing.Font("Roboto", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectImgBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(113)))), ((int)(((byte)(37)))));
+            this.selectImgBtn.IdleBorderThickness = 1;
+            this.selectImgBtn.IdleCornerRadius = 20;
+            this.selectImgBtn.IdleFillColor = System.Drawing.Color.White;
+            this.selectImgBtn.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(113)))), ((int)(((byte)(37)))));
+            this.selectImgBtn.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(113)))), ((int)(((byte)(37)))));
+            this.selectImgBtn.Location = new System.Drawing.Point(99, 360);
+            this.selectImgBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectImgBtn.Name = "selectImgBtn";
+            this.selectImgBtn.Size = new System.Drawing.Size(167, 55);
+            this.selectImgBtn.TabIndex = 14;
+            this.selectImgBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // InitialForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(973, 487);
             this.Controls.Add(this.panel1);
@@ -528,5 +561,7 @@
         private Bunifu.UI.WinForms.BunifuTextbox.BunifuTextBox shopNametxt;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuThinButton2 selectImgBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
